d3451371fd3da6ebe86bfe699720496908833a2d
marcelnoehre <marcelnoehre@web.de>
2025-05-08 11:09:26 +0200

    univariat mar

--- src/missingness.py
+++ src/missingness.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from mdatagen.univariate.uMCAR import uMCAR
 from mdatagen.univariate.uMNAR import uMNAR
+from mdatagen.univariate.uMAR import uMAR
 from src.utils import *
 
 def univariat_mcar(df, col, target_col, missing_rate, seed=None):
@@ -21,3 +22,13 @@ def univariat_mnar(df, col, target_col, threshold):
         x_miss=col
     )
     return generator.run()
+
+def univariat_umar(df, col_miss, col_obs, target_col, missing_rate):
+    generator = uMAR(
+        X=df.copy().drop(columns=[target_col]),
+        y=map_target_to_np_array(df, target_col),
+        missing_rate=missing_rate,
+        x_miss=col_miss,
+        x_obs=col_obs
+    )
+    return generator.mix()
