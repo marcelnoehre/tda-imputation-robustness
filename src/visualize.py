27b7b10718bc44a398635fa2f99fafae03995b40
marcelnoehre <marcelnoehre@web.de>
2025-05-08 19:40:11 +0200

    visualize introduced missingness

--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,9 @@
 import gudhi
+from mdatagen.plots import PlotMissingData
 
 def persistence_diagram(dataset):
     gudhi.plot_persistence_diagram(dataset)
+
+def plot_missing_data(data, missing_data, style="normal"):
+    miss_plot = PlotMissingData(data_missing=missing_data, data_original=data)
+    miss_plot.visualize_miss(style)
