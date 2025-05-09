9203f121e98d00eba07b4161e89321c75a4b1cee
marcelnoehre <marcelnoehre@web.de>
2025-05-09 10:13:27 +0200

    sync missingness formats

--- src/missingness.py
+++ src/missingness.py
@@ -15,7 +15,7 @@ def univariat_mcar(df, col, target_col, missing_rate, seed=None):
         x_miss=col,
         seed=seed
     )
-    return generator.random()
+    return generator.random().drop(columns=["target"])
 
 def univariat_mnar(df, col, target_col, threshold):
     generator = uMNAR(
@@ -24,7 +24,7 @@ def univariat_mnar(df, col, target_col, threshold):
         threshold=threshold,
         x_miss=col
     )
-    return generator.run()
+    return generator.run().drop(columns=["target"])
 
 def univariat_mar(df, col_miss, col_obs, target_col, missing_rate):
     generator = uMAR(
@@ -34,7 +34,7 @@ def univariat_mar(df, col_miss, col_obs, target_col, missing_rate):
         x_miss=col_miss,
         x_obs=col_obs
     )
-    return generator.mix()
+    return generator.mix().drop(columns=["target"])
 
 def multivariat_mcar(df, target_col, missing_rate, seed=None):
     generator = mMCAR(
@@ -51,7 +51,7 @@ def multivariat_mnar(df, target_col, threshold):
         y=map_target_to_np_array(df, target_col),
         threshold=threshold
     )
-    return generator.random()
+    return generator.random().drop(columns=["target"])
 
 def multivariat_mar(df, target_col, n_xmiss):
     generator = mMAR(
@@ -59,4 +59,4 @@ def multivariat_mar(df, target_col, n_xmiss):
         y=map_target_to_np_array(df, target_col),
         n_xmiss=n_xmiss
     )
-    return generator.random()
+    return generator.random().drop(columns=["target"])
