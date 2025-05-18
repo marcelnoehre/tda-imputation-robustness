e8551b39bc671cbaa9e00c7a8cb5e1817e8ed7b0
marcelnoehre <marcelnoehre@web.de>
2025-05-18 12:44:15 +0200

    introduce missingness

---
+++ src/missingness.py
@@ -0,0 +1,30 @@
+from src.constants import SEED, MEDIAN
+from src.utils import numeric_target_mapping
+from mdatagen.multivariate.mMAR import mMAR
+from mdatagen.multivariate.mMNAR import mMNAR
+from mdatagen.multivariate.mMCAR import mMCAR
+
+def multivariat_mcar(df, target, missing_rate):
+    generator = mMCAR(
+        X=df,
+        y=numeric_target_mapping(target),
+        missing_rate=missing_rate,
+        seed=SEED
+    )
+    return generator.random()
+
+def multivariat_nmar(df, target, missing_rate):
+    generator = mMNAR(
+        X=df,
+        y=numeric_target_mapping(target),
+        threshold=MEDIAN
+    )
+    return generator.MBOV_median(missing_rate, df.columns.tolist())
+
+def multivariat_mar(df, target, missing_rate):
+    generator = mMAR(
+        X=df,
+        y=numeric_target_mapping(target),
+        n_xmiss=4
+    )
+    return generator.random(missing_rate)
---
+++ src/utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def numeric_target_mapping(target):
+    return np.array([{cls: id for id, cls in enumerate(target)}[cls] for cls in target])
