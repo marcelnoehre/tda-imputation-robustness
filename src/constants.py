6570fd4b2893f0f6ff0dee5749da6f0929aab67d
marcelnoehre <marcelnoehre@web.de>
2025-05-18 15:10:53 +0200

    visualize the distance over missingrates

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,11 @@
+import numpy as np
+
 ### Analysis ###
 SEED = 42
 MEDIAN = 0.5
 
 ### Datasets ###
 IRIS_DATASET_ID = 61
+
+### Pipeline ###
+MISSING_RATES = np.arange(2, 50, 2)
--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,19 @@
 import gudhi
+import matplotlib.pyplot as plt
+from src.constants import MISSING_RATES
 
 def persistence_diagram(dataset):
     gudhi.plot_persistence_diagram(dataset)
+
+def plot_distance_over_missingrates(distances, distance_type):
+    plt.figure(figsize=(8,5))
+    plt.plot(MISSING_RATES, distances[0], marker='o', color='blue', label='H0')
+    plt.plot(MISSING_RATES, distances[1], marker='o', color='orange', label='H1')
+    plt.plot(MISSING_RATES, distances[2], marker='o', color='green', label='H2')
+
+    plt.xlabel('Missing Rate')
+    plt.ylabel(f'{distance_type} Distance')
+    plt.title(f'{distance_type} Distance over Missing Rates by Dimension')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
