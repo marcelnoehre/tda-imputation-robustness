4c0e289b35f21ae641d9ee12f50a8d5cb48e4a9a
marcelnoehre <marcelnoehre@web.de>
2025-06-19 09:42:14 +0200

    logger

---
+++ src/logger.py
@@ -0,0 +1,17 @@
+import datetime
+
+def _current_time():
+    """
+    Get the current time in the format of [YYYY-MM-DD HH:MM:SS].
+
+    :return str: Current timestamp
+    """
+    return datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+
+def log(msg):
+    """
+    Log a information with timestamp
+
+    :param msg: The message to be logged
+    """
+    print(f"\x1b[32;20m{_current_time()}\x1b[37;20m | \x1b[0m{msg}")
