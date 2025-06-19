d13429eb600473b6e99df3e812c712eeae16965c
marcelnoehre <marcelnoehre@web.de>
2025-06-19 14:51:38 +0200

    RQ1, RQ2, RQ3

---
+++ main.py
@@ -0,0 +1,23 @@
+from src.constants import *
+from src.pipeline import experiment
+
+def main():
+    experiment(
+        'impact_imputation_methods',
+        [MAR],
+        [5, 10, 25], 
+        [CONSTANT, MEAN, MEDIAN, KNN, RF, MICE], 
+        [VR],
+        [WS, BN, L2PL, L2PI]
+    )
+    experiment(
+        'impact_missingness_types_rates',
+        [MCAR, MAR, MNAR],
+        [1, 2, 3, 4, 5, 6, 8, 10, 15, 20, 25, 30, 40],
+        [KNN],
+        [VR],
+        [WS, BN, L2PL, L2PI]
+    )
+
+if __name__ == '__main__':
+    main()
