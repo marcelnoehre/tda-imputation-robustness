691dc6b07220d04df16db98423f595c27b806607
marcelnoehre <marcelnoehre@web.de>
2025-10-05 18:12:24 +0200

    refactor visualizations

--- src/visualize.py
+++ src/visualize.py
@@ -1,8 +1,9 @@
 import gudhi
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 from tabulate import tabulate
 from IPython.display import display, Markdown
@@ -112,16 +113,16 @@ def plot_data(type, ax, mean, sem, label, color, x=None, width=None, linestyle='
 def format_axes(type, ax, x_label, y_label, legend=False, title=False, title_label=None, ticks=None, tick_labels=None):
     if type == MISSING_RATE or type == MISSINGNESS_TYPE:
         if legend:
-            ax.legend(fontsize=12)
+            ax.legend(fontsize=14)
     elif type == IMPUTATION_METHOD or type == TDA_METHOD:
         ax.set_xticks(ticks)
-        ax.set_xticklabels(tick_labels, fontsize=12)
+        ax.set_xticklabels(tick_labels, fontsize=14)
 
     if title:
-            ax.set_title(title_label, fontsize=16, weight='bold')
-    ax.set_xlabel(x_label, fontsize=14)
-    ax.set_ylabel(y_label, fontsize=14)
-    ax.grid(True)
+        ax.set_title(title_label, fontsize=18, weight='bold')
+    ax.set_xlabel(x_label, fontsize=16)
+    ax.set_ylabel(y_label, fontsize=16)
+    ax.grid(True, alpha=0.7)
 
 def legend(axes, fig, cols, bbox):
     handles, labels = axes[0].get_legend_handles_labels()
@@ -129,7 +130,7 @@ def legend(axes, fig, cols, bbox):
         handles, labels,
         loc='lower center',
         ncol=cols,
-        fontsize=12,
+        fontsize=14,
         bbox_to_anchor=bbox
     )
 
@@ -139,11 +140,11 @@ def multi_legend(fig):
             Line2D([0], [0], color=plt.get_cmap(COLOR_MAP[DIMENSION])(dim), lw=2) 
             for dim in DIMENSIONS
         ],
-        labels=[f'{LABEL[DIMENSION]} {dim}' for dim in DIMENSIONS],
+        labels=[LABEL[dim] for dim in DIMENSIONS],
         loc='lower center',
         ncol=len(DIMENSIONS),
         frameon=False,
-        fontsize=12,
+        fontsize=14,
         bbox_to_anchor=(0.5, -0.04)
     )
 
@@ -156,13 +157,13 @@ def multi_legend(fig):
         loc='lower center',
         ncol=len(COLLECTIONS[MISSINGNESS_TYPE]),
         frameon=False,
-        fontsize=12,
+        fontsize=14,
         bbox_to_anchor=(0.5, -0.07)
     )
 
 def plot(title=''):
     plt.title(title)
-    plt.tight_layout(h_pad=2, w_pad=3)
+    plt.tight_layout(h_pad=2, w_pad=2)
     plt.show()
     plt.close()
 
@@ -177,63 +178,96 @@ def plot_missing_data(missing_data, original_data, type, title=''):
     miss_plot.visualize_miss(type, save=False)
     plot(title)
 
-def plot_persistence_diagram(data, title='', band=0.0, xlim=None, ylim=None, file=False):
+def plot_persistence_diagram(data, fontsize, legend=True, title='', band=0.0, xlim=None, ylim=None, file=False):
     if file:
-        ax = gudhi.plot_persistence_diagram(persistence_file=data, band=band)
+        gudhi.plot_persistence_diagram(persistence_file=data, band=band)
     else:
         max_dim = int(max(item[2] for item in data[0]))
         persistence_intervals = [
             [item[0:2] for item in data[0] if item[2] == dim]
             for dim in range(max_dim + 1)
         ]
-        ax = gudhi.plot_persistence_diagram(persistence_intervals, band=band)
+        gudhi.plot_persistence_diagram(persistence_intervals, band=band)
 
-    ax.set_title(title)
-    ax.grid(True)
+    if not legend:
+        plt.legend().remove()
+    else:
+        cmap = cm.get_cmap("Set1")
+        colors = [cmap(0), cmap(1), cmap(2)]
+        plt.legend(handles=[
+            Patch(color=colors[0], label=r"$H_{0}$"),
+            Patch(color=colors[1], label=r"$H_{1}$"),
+            Patch(color=colors[2], label=r"$H_{2}$")
+        ], title="Dimension", fontsize=fontsize, title_fontsize=fontsize)
+    
+    plt.xlabel("Birth", fontsize=fontsize + 2)
+    plt.ylabel("Death", fontsize=fontsize + 2)
+    plt.title(title)
+    plt.grid(True, alpha=0.7)
+    
+    
     if xlim is not None:
-        ax.set_xlim(xlim)
+        plt.xlim(xlim)
     if ylim is not None:
-        ax.set_ylim(ylim)
+        plt.ylim(ylim)
 
-def plot_barcode_diagram(data, title_0, title_1_2):
+def plot_barcode_diagram(data, title_0, title_1_2, fontsize):
     H0 = [(int(row[0]), (row[1], row[2])) for row in data if int(row[0]) == 0]
     H1_H2 = [(int(row[0]), (row[1], row[2])) for row in data if int(row[0]) in [1, 2]]
-    gudhi.plot_persistence_barcode(H0, legend=True)
+    cmap = cm.get_cmap("Set1")
+    colors = [cmap(0), cmap(1), cmap(2)]
+    gudhi.plot_persistence_barcode(H0, legend=True, fontsize=fontsize)
+    plt.legend(handles=[
+        Patch(color=colors[0], label=r"$H_{0}$")
+    ], title="Dimension", fontsize=fontsize, title_fontsize=fontsize)
     plt.title(title_0)
-    plt.grid(True)
-    gudhi.plot_persistence_barcode(H1_H2, legend=True)
+    plt.grid(True, alpha=0.7)
+    gudhi.plot_persistence_barcode(H1_H2, legend=True, fontsize=fontsize)
+    plt.legend(handles=[
+        Patch(color=colors[1], label=r"$H_{1}$"),
+        Patch(color=colors[2], label=r"$H_{2}$")
+    ], title="Dimension", fontsize=fontsize, title_fontsize=fontsize)
     plt.ylim(len(H1_H2) + 2)
     plt.title(title_1_2)
-    plt.grid(True)
+    plt.grid(True, alpha=0.7)
 
-def plot_persistence_landscape(diagrams, title=''):
+def plot_persistence_landscape(diagrams, layers=1, title=''):
+    colors = [c for c in ['red', 'blue', 'green'] for _ in range(layers)]
     for k, l in enumerate(diagrams):
-        plt.plot(l, color=['red', 'blue', 'green'][k], label=f'$\\lambda_{{{k+1}}}$')
+        if layers == 1:
+            label = f'$\\lambda_{{{k+1}}}$'
+        else:
+            if k % layers == 0:
+                label = f'$\\lambda_{{{(k // layers) * layers + 1}-{((k // layers) + 1) * layers}}}$'
+            else:
+                label = None
+
+        plt.plot(l, color=colors[k], label=label)
 
     plt.title(title)
-    plt.xlabel('$\\varepsilon$')
-    plt.ylabel('$\\lambda_k(\\varepsilon)$')
-    plt.legend()
-    plt.grid(True)
+    plt.xlabel('$\\varepsilon$', fontsize=16)
+    plt.ylabel('$\\lambda_k(\\varepsilon)$', fontsize=16)
+    plt.legend(fontsize=14)
+    plt.grid(True, alpha=0.7)
     plt.show()
 
-def plot_persistence_image(persistent_images, sample_idx = 0, title=''):
-    n_dims = persistent_images.shape[1]
-    fig, axes = plt.subplots(1, n_dims, figsize=(5 * n_dims, 4))
+def plot_persistence_image(persistent_images, title=''):
+    n_dims = persistent_images.shape[0]
+    fig, axes = plt.subplots(1, n_dims, figsize=(4 * n_dims, 4))
+    plt.subplots_adjust(wspace=0.05, hspace=0)
 
     for i in range(n_dims):
         ax = axes[i] if n_dims > 1 else axes
-        ax.imshow(persistent_images[sample_idx, i])
-        ax.set_title(f'H{i}')
+        ax.imshow(persistent_images[i])
         ax.axis('off')
+        ax.text(0.5, -0.025, f'$H_{i}$', fontsize=14, ha='center', va='top', transform=ax.transAxes)
 
     if title:
-        fig.suptitle(title, fontsize=16)
+        fig.suptitle(title, fontsize=20)
 
-    plt.tight_layout(rect=[0, 0, 1, 0.95])
     plt.show()
 
-def persistence_diagram_missing_rate(dataset):
+def persistence_diagram_missing_rate(dataset, band):
     dataset_mr = {}
     dataset_mr[0] = dataset[DATA]
     for mr in [5, 10, 25]:
@@ -253,4 +287,4 @@ def persistence_diagram_missing_rate(dataset):
     xlim = (min(births) - padding, max(deaths) + padding)
     ylim = (min(births) - padding, max(deaths) + padding)
     for mr, diagrams in diagrams_mr.items():
-        plot_persistence_diagram(diagrams, band=0.075, xlim=xlim, ylim=ylim)
+        plot_persistence_diagram(diagrams, fontsize=14, legend=mr == 0, band=band, xlim=xlim, ylim=ylim)
